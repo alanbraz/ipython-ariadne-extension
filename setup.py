69ca19150a1917a83b864095493431061c5cd4a6
Alan Braz <braz.alan@ibm.com>
2018-07-24 17:10:48 -0400

    rename package to ariadne

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 import setuptools
-from typecheck import __version__
+from ariadne import __version__
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ipython-rest-extension",
+    name="ipython-ariadne-extension",
     version=__version__,
     author="Alan Braz",
     author_email="alanbraz@gmail.com",
-    description="IPython typecheck extension for notebook\' multiple cells.",
+    description="IPython WALA/ML (Ariadne) extension for notebook\'s multiple cells.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/alanbraz/ipython-rest-extension",
+    url="https://github.com/alanbraz/ipython-ariadne-extension",
     packages=setuptools.find_packages(),
     #packages=['typecheck-extension'],
     # install_requires=['mypy'],
