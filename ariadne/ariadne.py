e733cd96d59d9ca91c44798668c22061a8fdcc1d
Alan Braz <braz.alan@ibm.com>
2018-07-25 10:12:47 -0400

    str numbers on debug messages

--- ariadne/ariadne.py
+++ ariadne/ariadne.py
@@ -68,13 +68,13 @@ class Ariadne(object):
                 for diag in diagnostic:
                     if diag["severity"] == "Error":
                         error_column_number = diag["range"]["start"]["character"]
-                        print("Adriane DEBUG error_column_number: " + error_column_number, file=sys.stderr)
+                        print("Adriane DEBUG error_column_number: " + str(error_column_number), file=sys.stderr)
 
                         error_line_number = diag["range"]["start"]["line"]
-                        print("Adriane DEBUG error_line_number: " + error_line_number, file=sys.stderr)
+                        print("Adriane DEBUG error_line_number: " + str(error_line_number), file=sys.stderr)
 
                         error_cell_line_number = error_line_number - len(("\n".join(self.ok_cells)).split("\n"))
-                        print("Adriane DEBUG error_cell_line_number: " + error_cell_line_number, file=sys.stderr)
+                        print("Adriane DEBUG error_cell_line_number: " + str(error_cell_line_number), file=sys.stderr)
 
                         line_label = "Line "+ str(error_cell_line_number) + ": "
                         error_message = "Adriane diagnostic error:\n"
@@ -86,7 +86,7 @@ class Ariadne(object):
 
                         error_message += "\n" + len(line_label)*" "+ (error_column_number-1)*" " + "^"
                         print("Adriane DEBUG:\n"+json.dumps(diag, indent=2), file=sys.stderr)
-                        
+
                 if error_message is not None:
                     print(error_message, file=sys.stderr)
                 else:
