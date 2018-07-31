bb070f2b8d0e34ecb37380f8373dd3e700c59f3b
Alan Braz <braz.alan@ibm.com>
2018-07-31 15:15:00 -0400

    more debug

--- ariadne/ariadne.py
+++ ariadne/ariadne.py
@@ -91,7 +91,7 @@ class Ariadne(object):
                         error_message += line_label + current_cell_array[error_cell_line_number]
                         # print("Adriane DEBUG: cells_to_run: " + cells_to_run, file=sys.stderr
                         error_message += "\n" + len(line_label)*" "+ (error_column_number)*" " + (error_end_column_number-error_column_number)*"^"
-                        # print("Adriane DEBUG::\n"+json.dumps(diag, indent=2), file=sys.stderr)
+                        print("Adriane DEBUG::\n"+json.dumps(diag, indent=2), file=sys.stderr)
                         if error_message is not None:
                             print(error_message, file=sys.stderr)
                         else:
